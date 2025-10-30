bf94a686afd23e835aa1db17387add943c0ae138
santhiraju0 <santhi.r@netlabsglobal.com>
2025-10-30 08:14:56 +0530

    changed to cloud db

--- config/db.js
+++ config/db.js
@@ -1,5 +1,35 @@
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT || 3306,
+//     dialect: "mysql",
+//     logging: false,
+//   }
+// );
+
+// module.exports = sequelize;
+
+// db.js
 const { Sequelize } = require("sequelize");
+const fs = require("fs");
 require("dotenv").config();
+
+const dialectOptions = {};
+
+// if you downloaded the CA, include it:
+if (process.env.AIVEN_CA_PATH) {
+  dialectOptions.ssl = {
+    ca: fs.readFileSync(process.env.AIVEN_CA_PATH)
+  };
+  // Some environments may need:
+  dialectOptions.ssl.rejectUnauthorized = true;
+}
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -10,6 +40,7 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT || 3306,
     dialect: "mysql",
     logging: false,
+    dialectOptions
   }
 );
 
